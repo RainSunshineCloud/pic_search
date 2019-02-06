9d33ff7f6da4f07c7eb14ef7c06f82ca9be7bdb1
ryanwu <931035553@qq.com>
2019-02-06 17:56:57 +0800

    图片模糊搜索

--- example/test.php
+++ example/test.php
@@ -1,5 +1,5 @@
 <?php 
-include __DIR__.'/../vendor/autoload.php';
+include __DIR__.'/../../../vendor/autoload.php';
 $file = ['./2.jpg','./3.jpg','./4.jpg','./5.jpg','./6.jpg','./7.jpg','./8.jpg'];
 
 $pic = new RainSunshineCloud\picSearch('./1.jpg');
